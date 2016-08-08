986d9661a25fe62f8f29db7e9f79f34e81cb222d
Walter dos Santos Filho <waltersf@gmail.com>
2016-08-08 16:35:26 -0300

    Implementing validation of request body and implementation of execute method to support task and parameters specification

---
+++ tahiti/execution_api.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-}
+from flask import request
+from flask_restful import Resource
+
+from app_auth import requires_auth
+from models import db, Execution
+from schema import *
+
+# region Protected
+# endregion
+
+
+class ExecutionListApi(Resource):
+    """ REST API for listing class Execution """
+
+    @staticmethod
+    @requires_auth
+    def get():
+        executions = Execution.query.all()
+        return ExecutionListResponseSchema(many=True).dump(executions).data
+
+
+class ExecutionDetailApi(Resource):
+    """ REST API for a single instance of class Execution """
+
+    @staticmethod
+    @requires_auth
+    def get(execution_id):
+        execution = Execution.query.get(execution_id)
+        if execution is not None:
+            return ExecutionItemResponseSchema().dump(execution).data
+        else:
+            return dict(status="ERROR", message="Not found"), 404
+
+    @staticmethod
+    @requires_auth
+    def patch(execution_id):
+        if request.json:
+            request_schema = ExecutionCreateRequestSchema(partial=True)
+            form = request_schema.load(request.json)
+            if not form.errors:
+                Execution.query.filter_by(id=execution_id).update(**form.data)
+                db.session.commit()
+                execution = Execution.query.get(execution_id)
+                if execution is not None:
+                    return dict(status="OK", message="Updated")
+                else:
+                    return dict(status="ERROR", message="Not found"), 404
+            else:
+                return dict(status="ERROR", message="Invalid data",
+                            erros=form.errors), 404
+        else:
+            return dict(status="ERROR", message="Insufficient data"), 404
