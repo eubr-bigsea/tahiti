08f3b1a473bafbc923376d45fd906ec5e72d6d0f
Walter dos Santos Filho <waltersf@gmail.com>
2017-06-27 17:16:13 -0300

    Fixes in migration for operation scripts.
    - Enabling operations
    - Changed some categories for operations

--- migrations/versions/2af6ef956270_adding_operation_scripts.py
+++ migrations/versions/2af6ef956270_adding_operation_scripts.py
@@ -21,40 +21,41 @@ depends_on = None
 
 def _insert_scripts():
     tb = table(
-        'operation_category_translation',
+        'operation_script',
         column('id', Integer),
+        column('enabled', Integer),
         column('type', String),
-        column('script', String),
+        column('body', String),
         column('operation_id', Integer),
     )
 
     columns = [c.name for c in tb.columns]
     data = [
-        (1, "JS_CLIENT", "copyInputAddField(task, 'prediction', false);", 42),
-        (2, "JS_CLIENT", "copyInputAddField(task, 'alias', false);", 7),
-        (3, "JS_CLIENT", "copyInputAddField(task, 'alias', false);", 41),
-        (4, "JS_CLIENT", "onlyField(task, 'attributes', true)", 6),
-        (5, "JS_CLIENT", "copyInput(task);", 37),
-        (6, "JS_CLIENT", "copyInput(task);", 5),
-        (7, "JS_CLIENT", "copyInput(task);", 13),
-        (8, "JS_CLIENT", "copyInput(task);", 17),
-        (9, "JS_CLIENT", "copyInput(task);", 21),
-        (10, "JS_CLIENT", "copyInput(task);", 23),
-        (11, "JS_CLIENT", "copyInput(task);", 27),
-        (12, "JS_CLIENT", "copyInput(task);", 28),
-        (13, "JS_CLIENT", "copyInput(task);", 32),
-        (14, "JS_CLIENT", dedent("""
+        (1, 1, "JS_CLIENT", "copyInputAddField(task, 'prediction', false);", 42),
+        (2, 1, "JS_CLIENT", "copyInputAddField(task, 'alias', false);", 7),
+        (3, 1, "JS_CLIENT", "copyInputAddField(task, 'alias', false);", 41),
+        (4, 1, "JS_CLIENT", "onlyField(task, 'attributes', true)", 6),
+        (5, 1, "JS_CLIENT", "copyInput(task);", 37),
+        (6, 1, "JS_CLIENT", "copyInput(task);", 5),
+        (7, 1, "JS_CLIENT", "copyInput(task);", 13),
+        (8, 1, "JS_CLIENT", "copyInput(task);", 17),
+        (9, 1, "JS_CLIENT", "copyInput(task);", 21),
+        (10, 1, "JS_CLIENT", "copyInput(task);", 23),
+        (11, 1, "JS_CLIENT", "copyInput(task);", 27),
+        (12, 1, "JS_CLIENT", "copyInput(task);", 28),
+        (13, 1, "JS_CLIENT", "copyInput(task);", 32),
+        (14, 1, "JS_CLIENT", dedent("""
             task.uiPorts.output = task.forms.attributes.value.slice()
             Array.prototype.push.apply(task.uiPorts.output,
                 task.forms.function.value.map((v) = > {
                     return v.alias}));"""), 15),
-        (15, "JS_CLIENT",
+        (15, 1, "JS_CLIENT",
          "copyInputAddAttributesSplitAlias(task, 'attributes', 'alias');", 40),
-        (16, "JS_CLIENT",
+        (16, 1, "JS_CLIENT",
          ("copyFromOnlyOneInput(task, 5); "
           "//hard coded id for 'input data 1', first input data port"), 12),
-        (17, "JS_CLIENT", "joinSuffixDuplicatedAttributes(task);", 24),
-        (18, "JS_CLIENT", "joinSuffixDuplicatedAttributes(task);", 16),
+        (17, 1, "JS_CLIENT", "joinSuffixDuplicatedAttributes(task);", 24),
+        (18, 1, "JS_CLIENT", "joinSuffixDuplicatedAttributes(task);", 16),
     ]
     rows = [dict(zip(columns, row)) for row in data]
     op.bulk_insert(tb, rows)
@@ -64,9 +65,13 @@ all_commands = [
     (_insert_scripts, 'DELETE FROM operation_script '),
     ('UPDATE operation SET enabled = 1 WHERE id = 2',
      'UPDATE operation SET enabled = 0 WHERE id = 2'),
-    ('INSERT INTO operation_category_category VALUES(53, 3)',
-     '''DELETE FROM operation_category_category WHERE operation_id = 53
-     AND category_id = 3 '''),
+    ('''DELETE FROM operation_category_operation WHERE operation_id = 30
+     AND operation_category_id = 3 ''',
+        'INSERT INTO operation_category_operation VALUES(30, 3)'
+     ),
+    ('INSERT INTO operation_category_operation VALUES(53, 3)',
+     '''DELETE FROM operation_category_operation WHERE operation_id = 53
+     AND operation_category_id = 3 '''),
 ]
 
 
