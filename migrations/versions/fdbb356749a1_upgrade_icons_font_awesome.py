68149f8624b6bef8948ab8c672d229645fcb954f
Matheus Gonçalves <souzagonc@gmail.com>
2018-03-27 15:08:48 -0300

    fix migrations of icons

--- migrations/versions/fdbb356749a1_upgrade_icons_font_awesome.py
+++ migrations/versions/fdbb356749a1_upgrade_icons_font_awesome.py
@@ -24,16 +24,14 @@ def upgrade():
         [58, "fa-external-link-square", "fa-external-link-alt"],
         [12, "icon-union", "fa-plus-square"],
         [93, "fa-flash", "fa-bolt"],
-        [6, "icon-projection", "fa-column"],
+        [6, "icon-projection", "fa-columns"],
         [13, "icon-intersection", "fa-compress"],
         [17, "fa-code-fork", "fa-code-branch"],
-        [36, "fa-lemon-o", "fa-play"],
         [71, "fa-area-chart", "fa-chart-area"],
         [69, "fa-bar-chart", "fa-chart-bar"],
         [89, "fa-circle-o-notch", "fa-circle-notch"],
         [68, "fa-line-chart", "fa-chart-line"],
         [70, "fa-pie-chart", "fa-chart-pie"],
-        [87, "fa-lemon-o", "fa-braille"],
         [53, "fa-map-o", "fa-file"],
         [85, "fa-long-arrow-right", "fa-list-ol"],
         [86, "fa-diamond", "fa-gem"],
@@ -50,14 +48,29 @@ def upgrade():
         [22, "fa-cloud-download", "fa-cloud-download-alt"],
         [39, "fa-cloud-upload", "fa-cloud-upload-alt"],
         [77, "fa-id-card-o", "fa-id-card"],
-        [8, "fa-line-chart", "fa-chart-line"],
         [73, "fa-shield", "fa-shield-alt"],
         [25, "fa-commenting-o", "fa-comment-alt"],
         [83, "fa-vcard-o", "fa-address-card"],
         [20, "fa-gears", "fa-cogs"],
         [52, "fa-long-arrow-right", "fa-long-arrow-alt-right"],
-        [50, "fa-hand-stop-o", "fa-hand-paper"]
+        [50, "fa-hand-stop-o", "fa-hand-paper"],
+        [99, "fa-picture-o", "fa-image"]
     ]
+
+    connection.execute("""
+        UPDATE operation
+        SET icon = 'fa-tachometer-alt'
+        WHERE id = 26""")
+
+    connection.execute("""
+        UPDATE operation
+        SET icon = 'fa-play'
+        WHERE id = 36""")
+
+    connection.execute("""
+        UPDATE operation
+        SET icon = 'fa-braille'
+        WHERE id = 87""")
 
     for op in data:
         connection.execute("""
@@ -76,10 +89,9 @@ def downgrade():
         [58, "fa-external-link-square", "fa-external-link-alt"],
         [12, "icon-union", "fa-plus-square"],
         [93, "fa-flash", "fa-bolt"],
-        [6, "icon-projection", "fa-column"],
+        [6, "icon-projection", "fa-columns"],
         [13, "icon-intersection", "fa-compress"],
         [17, "fa-code-fork", "fa-code-branch"],
-        [36, "fa-lemon-o", "fa-play"],
         [71, "fa-area-chart", "fa-chart-area"],
         [69, "fa-bar-chart", "fa-chart-bar"],
         [89, "fa-circle-o-notch", "fa-circle-notch"],
@@ -108,8 +120,24 @@ def downgrade():
         [83, "fa-vcard-o", "fa-address-card"],
         [20, "fa-gears", "fa-cogs"],
         [52, "fa-long-arrow-right", "fa-long-arrow-alt-right"],
-        [50, "fa-hand-stop-o", "fa-hand-paper"]
+        [50, "fa-hand-stop-o", "fa-hand-paper"],
+        [99, "fa-picture-o", "fa-image"]
     ]
+
+    connection.execute("""
+        UPDATE operation
+        SET icon = 'fa-dashboard'
+        WHERE id = 26""")
+
+    connection.execute("""
+        UPDATE operation
+        SET icon = 'fa-lemon-o'
+        WHERE id = 36""")
+
+    connection.execute("""
+        UPDATE operation
+        SET icon = 'fa-lemon-o'
+        WHERE id = 87""")
 
     for op in data:
         connection.execute("""
