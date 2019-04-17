ab870a134367db9da228fa3e7553cfadad64c568
Walter dos Santos Filho <waltersf@gmail.com>
2019-04-17 11:07:04 -0300

    Fixing problems with dependencies security

--- tahiti/configuration.py
+++ tahiti/configuration.py
@@ -6,7 +6,7 @@ import yaml
 
 def load():
     with open(os.environ.get('TAHITI_CONFIG')) as f:
-        config = yaml.load(f.read())
+        config = yaml.load(f.read(), Loader=yaml.FullLoader)
     return config
 
 tahiti_configuration = load()
