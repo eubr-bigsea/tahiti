bb64690565bd122685e01bfe8371ac8f0f84238e
Laura Andrade <laura.nunes.andrade@gmail.com>
2019-10-04 17:15:52 -0300

    Fixing blank category scikit-learn

---
+++ migrations/versions/86dfd72f87d9_testes.py
@@ -0,0 +1,72 @@
+"""Testes
+
+Revision ID: 86dfd72f87d9
+Revises: 93084d865c9e
+Create Date: 2019-10-03 14:49:33.659142
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from alembic import context
+from alembic import op
+from sqlalchemy import String, Integer, Text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import table, column, text
+import json
+
+
+
+# revision identifiers, used by Alembic.
+revision = '86dfd72f87d9'
+down_revision = '93084d865c9e'
+branch_labels = None
+depends_on = None
+
+all_commands = [
+    ('DELETE FROM operation_category_operation WHERE operation_id = 3020 AND operation_category_id = 3001',""),
+    ('INSERT INTO operation_category_operation (operation_id, operation_category_id) VALUES (3020, 8)',""),
+]
+
+def upgrade():
+    ctx = context.get_context()
+    session = sessionmaker(bind=ctx.bind)()
+    connection = session.connection()
+
+    try:
+        connection.execute('SET FOREIGN_KEY_CHECKS=0;')
+        for cmd in all_commands:
+            if isinstance(cmd[0], str):
+                connection.execute(cmd[0])
+            elif isinstance(cmd[0], list):
+                for row in cmd[0]:
+                    connection.execute(row)
+            else:
+                cmd[0]()
+        connection.execute('SET FOREIGN_KEY_CHECKS=1;')
+    except:
+        session.rollback()
+        raise
+    session.commit()
+
+
+def downgrade():
+    ctx = context.get_context()
+    session = sessionmaker(bind=ctx.bind)()
+    connection = session.connection()
+
+    try:
+        connection.execute('SET FOREIGN_KEY_CHECKS=0;')
+        for cmd in reversed(all_commands):
+            if cmd[1]:
+                if isinstance(cmd[1], str):
+                    connection.execute(cmd[1])
+                elif isinstance(cmd[1], list):
+                    for row in cmd[1]:
+                        connection.execute(row)
+                else:
+                    cmd[1]()
+        connection.execute('SET FOREIGN_KEY_CHECKS=1;')
+    except:
+        session.rollback()
+        raise
+    session.commit()
