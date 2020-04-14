262a4bd2ed6834d5a8e760ceadf2397c3ae95baf
Walter dos Santos Filho <waltersf@gmail.com>
2020-04-13 21:46:43 -0300

    Adjusts for new Thorn

--- tahiti/app_auth.py
+++ tahiti/app_auth.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-}
 import json
 import logging
+import re
 from collections import namedtuple
 from functools import wraps
 
@@ -8,7 +9,7 @@ import requests
 from flask import request, Response, current_app, g as flask_g
 
 User = namedtuple(
-    "User", "id, login, email, name, first_name, last_name, locale, roles")
+    "User", "id, login, email, name, first_name, last_name, locale, permissions")
 
 MSG1 = 'Could not verify your access level for that URL. ' \
        'You have to login with proper credentials provided by Lemonade Thorn'
@@ -26,67 +27,23 @@ def authenticate(msg, params):
     return Response(json.dumps({'status': 'ERROR', 'message': msg}), 401,
                     mimetype="application/json")
 
-
 def requires_auth(f):
     @wraps(f)
     def decorated(*_args, **kwargs):
         config = current_app.config[CONFIG_KEY]
-        internal_token = request.args.get(
-            'token', request.headers.get('x-auth-token'))
-        authorization = request.headers.get('authorization')
         user_id = request.headers.get('x-user-id')
+        permissions = request.headers.get('x-permissions')
+        user_data = request.headers.get('x-user-data')
 
-        if authorization and user_id:
-            # It is using Thorn
-            url = '{}/api/tokens'.format(config['services']['thorn']['url'])
-            # url = 'http://localhost:3000/api/tokens'
-
-            payload = json.dumps({
-                'data': {
-                    'type': 'tokens',
-                    'id': user_id
-                }
-            })
-            headers = {
-                'content-type': "application/json",
-                'authorization': authorization,
-                'cache-control': "no-cache",
-            }
-            r = requests.request("POST", url, data=payload,
-                                 headers=headers)
-            if r.status_code != 200:
-                if internal_token and internal_token == str(config['secret']):
-                    setattr(flask_g, 'user', User(2, '', '', '', '', ''))
-                    log.warn('Using Authorization and token is incorrect!')
-                    return f(*_args, **kwargs)
-                else:
-                    print(('Error in authentication ({}, {}, {}): {}'.format(
-                        authorization, user_id, url, r.text)))
-                    log.error('Error in authentication ({}, {}, {}): {}'.format(
-                        authorization, user_id, url, r.text))
-                    return authenticate(MSG2, {})
-            else:
-                user_data = json.loads(r.text)
-                setattr(flask_g, 'user', User(
-                    id=int(user_id),
-                    name='{} {}'.format(
-                        user_data['data']['attributes']['first_name'],
-                        user_data['data']['attributes']['last_name']),
-                    login=user_data['data']['attributes']['email'],
-                    email=user_data['data']['attributes']['email'],
-                    first_name=user_data['data']['attributes']['first_name'],
-                    last_name=user_data['data']['attributes']['last_name'],
-                    locale=user_data['data']['attributes']['locale'],
-                    roles=user_data['data']['attributes']['roles']))
-                return f(*_args, **kwargs)
-        elif internal_token:
-            if internal_token == str(config['secret']):
-                # System user being used
-                setattr(flask_g, 'user', User(1, '', '', '', '', '', '', ''))
-                return f(*_args, **kwargs)
-            else:
-                return authenticate(MSG2, {"message": "Invalid X-Auth-Token"})
+        if all([user_data, user_id, permissions]):
+            login, email, name, locale = user_data.split(';')
+            setattr(flask_g, 'user', User(user_id, login, email, name, locale, '', '',
+                permissions.split(',')))
+            return f(*_args, **kwargs)
         else:
             return authenticate(MSG1, {'message': 'Invalid authentication'})
 
     return decorated
+
+
+
