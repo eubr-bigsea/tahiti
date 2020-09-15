48840e86774b852f9b180183256fb9f1984dc60d
Walter dos Santos Filho <waltersf@gmail.com>
2020-09-15 16:43:15 -0300

    Adjusts in migration version number

--- migrations/versions/6a299689ea75_adjusts_sept_2020.py
+++ migrations/versions/6a299689ea75_adjusts_sept_2020.py
@@ -1,8 +1,8 @@
-""" user filter  
+""" Adjusts sept 2020  
 
-Revision ID: 5529683a8d9b
-Revises: ed920c727112
-Create Date: 2020-06-18 20:06:05.438946
+Revision ID: 6a299689ea75
+Revises: b7442131c810
+Create Date: 2020-09-02 15:26:43.422785
 
 """
 from alembic import context
@@ -11,18 +11,26 @@ from sqlalchemy import String, Integer, Text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import table, column
 import sqlalchemy as sa
+import json
+
+from alembic import op
+import sqlalchemy as sa
+
 
 # revision identifiers, used by Alembic.
-revision = '5529683a8d9b'
-down_revision = 'ed920c727112'
+revision = '6a299689ea75'
+down_revision = 'b7442131c810'
 branch_labels = None
 depends_on = None
 
-USER_FILTER = 129
-PORT_RANGE = [306, 307]
-FORM_ID = 140
-FORM_FIELD_RANGE = [524, 526]
 
+BASE_FORM_ID = 151
+BASE_FORM_FIELD_ID = 574
+
+BASE_PORT_ID = 321
+DATA_SOURCE = 138
+
+ALL_OPS = [DATA_SOURCE]
 
 def _insert_operation():
     tb = table(
@@ -36,7 +44,7 @@ def _insert_operation():
         )
 
     rows = [
-            (USER_FILTER, 'user-filter', 1, 'TRANSFORMATION', 'fa-window-close-o', 'double-layout'),
+            (DATA_SOURCE, 'data-source', 1, 'SHORTCUT', ' ', None),
     ]
     rows = [dict(list(zip([c.name for c in tb.columns], row))) for row in rows]
 
@@ -51,10 +59,8 @@ def _insert_operation_translation():
         column('description', String), )
 
     rows = [
-        (USER_FILTER, 'en', 'User filter',
-         'Allows users to provide filters values before the execution.'),
-        (USER_FILTER, 'pt', 'Filtro do usuário',
-         'Permite que os usuários especifiquem filtros antes da execução.'),
+        (DATA_SOURCE, 'en', 'Data source', 'Reads a data source.'),
+        (DATA_SOURCE, 'pt', 'Fonte de dados', 'Lê uma fonte de dados.'),
     ]
     rows = [dict(list(zip([c.name for c in tb.columns], row))) for row in rows]
 
@@ -67,9 +73,7 @@ def _insert_operation_platform():
         column('operation_id', Integer),
         column('platform_id', Integer))
 
-    rows = [
-        (USER_FILTER, 1),
-    ]
+    rows = [(op_id, 1) for op_id in ALL_OPS]
     rows = [dict(list(zip([c.name for c in tb.columns], row))) for row in rows]
 
     op.bulk_insert(tb, rows)
@@ -86,32 +90,37 @@ def _insert_operation_port():
         column('multiplicity', String),
         column('slug', String))
 
-    rows = [
-        (306, 'INPUT', None, USER_FILTER, 1, 'ONE', 'input data'),
-        (307, 'OUTPUT', None, USER_FILTER, 1, 'MANY', 'output data'),
-    ]
+    new_id = BASE_PORT_ID
+    rows = []
+    for op_id in ALL_OPS:
+        new_id += 1
+        rows.append([new_id, 'OUTPUT', None, op_id, 1, 'MANY', 'output data'])
+        # new_id += 1
+        # rows.append([new_id, 'INPUT', None, op_id, 1, 'ONE', 'input data'])
+
     rows = [dict(list(zip([c.name for c in tb.columns], row))) for row in rows]
-
     op.bulk_insert(tb, rows)
+    
+def _insert_operation_port_translation():    
 
-
-def _insert_operation_port_translation():
     tb = table(
         'operation_port_translation',
         column('id', Integer),
         column('locale', String),
         column('name', String),
         column('description', String), )
+    new_id = BASE_PORT_ID
+    data = []
+    for op_id in ALL_OPS:
+        new_id += 1
+        data.append([new_id, 'en', 'output data', 'Output data'])
+        data.append([new_id, 'pt', 'dados de saída', 'Dados de saída'])
+        # new_id += 1
+        # data.append([new_id, 'en', 'input data', 'Input data'])
+        # data.append([new_id, 'pt', 'dados de entrada', 'Dados de entrada'])
 
-    rows = [
-        (306, 'en', 'input data', 'Input data'),
-        (306, 'pt', 'dados de entrada', 'Dados de entrada'),
-
-        (307, 'en', 'output data', 'Output data'),
-        (307, 'pt', 'dados de saída', 'Dados de saída'),
-
-    ]
-    rows = [dict(list(zip([c.name for c in tb.columns], row))) for row in rows]
+    
+    rows = [dict(list(zip([c.name for c in tb.columns], row))) for row in data]
     op.bulk_insert(tb, rows)
 
 
@@ -122,30 +131,18 @@ def _insert_operation_port_interface_operation_port():
         column('operation_port_interface_id', Integer))
 
     columns = [c.name for c in tb.columns]
-    data = [
-        (306, 1),
-        (307, 1),
-    ]
+    
+    base_id_port = BASE_PORT_ID
+    new_id = base_id_port
+
+    data = []
+    for op_id in ALL_OPS:
+        new_id += 1
+        data.append([new_id, 1])
+        # new_id += 1
+        # data.append([new_id, 1])
+
     rows = [dict(list(zip(columns, cat))) for cat in data]
-    op.bulk_insert(tb, rows)
-
-
-def _insert_operation_script():
-    tb = table(
-        'operation_script',
-        column('id', Integer),
-        column('type', String),
-        column('enabled', Integer),
-        column('body', String),
-        column('operation_id', Integer))
-
-    columns = [c.name for c in tb.columns]
-    data = [
-        [72, 'JS_CLIENT', 1,
-         "copyInput(task);", USER_FILTER],
-    ]
-    rows = [dict(list(zip(columns, cat))) for cat in data]
-
     op.bulk_insert(tb, rows)
 
 
@@ -156,10 +153,9 @@ def _insert_operation_category_operation():
         column('operation_category_id', Integer))
 
     columns = [c.name for c in tb.columns]
-    data = [
-        (USER_FILTER, 1),
-        (USER_FILTER, 41),
-    ]
+    data = []
+    for op_id in ALL_OPS:
+        data.append([op_id, 1])
     rows = [dict(list(zip(columns, cat))) for cat in data]
 
     op.bulk_insert(tb, rows)
@@ -174,9 +170,13 @@ def _insert_operation_form():
         column('category', String), )
 
     columns = [c.name for c in tb.columns]
-    data = [
-        (FORM_ID, 1, 1, 'execution'),
-    ]
+
+    form_id = BASE_FORM_ID + 1
+    data = []
+    for op_id in ALL_OPS:
+        data.append([form_id, 1, 1, 'execution'])
+        form_id += 1
+
     rows = [dict(list(zip(columns, row))) for row in data]
 
     op.bulk_insert(tb, rows)
@@ -189,11 +189,14 @@ def _insert_operation_operation_form():
         column('operation_form_id', Integer))
 
     columns = [c.name for c in tb.columns]
-    data = [
-        (USER_FILTER, FORM_ID),
-        (USER_FILTER, 110),  # reports
-        (USER_FILTER, 41),  # appearance
-    ]
+
+    form_id = BASE_FORM_ID + 1
+    data = []
+    for op_id in ALL_OPS:
+        data.append([op_id, 41]) # appearance
+        data.append([op_id, form_id])
+        form_id += 1
+    
     rows = [dict(list(zip(columns, row))) for row in data]
 
     op.bulk_insert(tb, rows)
@@ -208,12 +211,16 @@ def _insert_operation_form_translation():
     )
 
     columns = [c.name for c in tb.columns]
-    data = [
-        (FORM_ID, 'en', 'Execution'),
-        (FORM_ID, 'pt', 'Execução'),
-    ]
-    rows = [dict(list(zip(columns, row))) for row in data]
 
+    form_id = BASE_FORM_ID + 1
+    data = []
+
+    for op_id in ALL_OPS:
+        data.append([form_id, 'en', 'Execution'])
+        data.append([form_id, 'pt', 'Execução'])
+        form_id += 1
+
+    rows = [dict(list(zip(columns, row))) for row in data]
     op.bulk_insert(tb, rows)
 
 
@@ -230,15 +237,30 @@ def _insert_operation_form_field():
         column('values_url', String),
         column('values', String),
         column('scope', String),
-        column('form_id', Integer), )
+        column('enable_conditions', String),
+        column('form_id', Integer), 
+        column('editable', Integer), 
+        )
 
+    JOIN_TYPES = [
+            {"en": "Inner join", "key": "inner", "value": "Inner join", "pt": "Inner join"}, 
+            {"en": "Left outer join", "key": "left_outer", "value": "Left outer join", "pt": "Left outer join"}, 
+            {"en": "Right outer join", "key": "right_outer", "value": "Right outer join", "pt": "Right outer join"}
+    ]
     data = [
-        [524, 'filters', 'TEXT', 1, 1, None,
-         'filter', None, None, 'EXECUTION', FORM_ID],
-        [525, 'ignore', 'INTEGER', 0, 2, None,
-         'checkbox', None, None, 'EXECUTION', FORM_ID],
-        [526, 'use_advanced_editor', 'INTEGER', 0, 3, None,
-         'checkbox', None, None, 'EXECUTION', FORM_ID],
+        # DATA SOURCE
+        [575, 'data_source', 'INTEGER', 1, 0, None, 'lookup', 
+            '`${LIMONERO_URL}/datasources?uiw=1&simple=true&list=true&enabled=1`', None, 'EXECUTION', None, 
+            BASE_FORM_ID + 1, 0],
+
+        # [571, 'join', 'TEXT', 0, 2, None, 'join-editor', None, None, 'EXECUTION', None, 
+        #     BASE_FORM_ID + 1],
+        # [572, 'join_type', 'TEXT', 0, 3, 'inner', 'dropdown', None, json.dumps(JOIN_TYPES), 'EXECUTION', None, 
+        #     BASE_FORM_ID + 1],
+        # [573, 'left_alias', 'TEXT', 0, 2, None, 'text', None, None, 'EXECUTION', None, 
+        #     BASE_FORM_ID + 1],
+        # [574, 'right_alias', 'TEXT', 0, 2, None, 'text', None, None, 'EXECUTION', None, 
+        #     BASE_FORM_ID + 1],
     ]
     columns = [c.name for c in tb.columns]
     rows = [dict(list(zip(columns, row))) for row in data]
@@ -255,58 +277,70 @@ def _insert_operation_form_field_translation():
 
     columns = [c.name for c in tb.columns]
     data = [
-        [524, 'en', 'Filters', 'Filters to be applied to the input.'],
-        [524, 'pt', 'Filtros', 'Filtros a serem aplicados à entrada.'],
-        [525, 'en', 'Ignore in design', 'In design/editing mode, filters are ignored.'],
-        [525, 'pt', 'Ignore ao executar em ambiente de edição', 
-            'Filtros são ignorados durante a edição do workflow.'],
-        [526, 'en', 'User can use advanced query editor', 'Users can define custom filters using an advanced query editor.'],
-        [526, 'pt', 'Disponibilizar editor de consulta', 'Usuários podem editar os filtros usando um editor avançado de consultas.'],
+        # Data Source
+        [575, 'en', 'Data source', 'Data source.'],
+        [575, 'pt', 'Fonte de dados', 'Fonte de dados.']
     ]
     rows = [dict(list(zip(columns, row))) for row in data]
     op.bulk_insert(tb, rows)
 
-
 all_commands = [
-    (_insert_operation, 'DELETE FROM operation WHERE id BETWEEN {s} AND {s}'.format(s=USER_FILTER)),
+
+     (
+         "ALTER TABLE operation MODIFY COLUMN `type` enum('ACTION','SHUFFLE','TRANSFORMATION','VISUALIZATION', 'SHORTCUT') COLLATE utf8_unicode_ci NOT NULL",
+         "ALTER TABLE operation MODIFY COLUMN `type` enum('ACTION','SHUFFLE','TRANSFORMATION','VISUALIZATION') COLLATE utf8_unicode_ci NOT NULL"
+     ),
+     (
+         "ALTER TABLE operation_form_field ADD COLUMN editable TINYINT(1) NOT NULL DEFAULT 1",
+         "ALTER TABLE operation_form_field DROP COLUMN editable",
+    ),
+    (_insert_operation, 'DELETE FROM operation WHERE id BETWEEN {s} AND {e}'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
 
     (_insert_operation_translation,
-     'DELETE FROM operation_translation WHERE id BETWEEN {s} AND {s}'.format(s=USER_FILTER)),
+     'DELETE FROM operation_translation WHERE id BETWEEN {s} AND {e}'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
     (_insert_operation_port,
      'DELETE FROM operation_port '
-     'WHERE (operation_id BETWEEN {s} AND {s})'.format(s=USER_FILTER)),
+     'WHERE operation_id BETWEEN {s} AND {e}'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
 
     (_insert_operation_port_translation,
      'DELETE FROM operation_port_translation WHERE id IN '
      '(SELECT id FROM operation_port '
-     '  WHERE (operation_id BETWEEN {s} AND {s}))'.format(s=USER_FILTER)),
-    (_insert_operation_script,
-     'DELETE FROM operation_script WHERE operation_id BETWEEN {s} AND {s}'.format(s=USER_FILTER),
-     ),
+     '  WHERE operation_id BETWEEN {s} AND {e})'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
+
     (_insert_operation_port_interface_operation_port,
      'DELETE FROM operation_port_interface_operation_port '
      'WHERE operation_port_id IN (SELECT id FROM operation_port '
-     'WHERE operation_id BETWEEN {s} AND {s})'.format(s=USER_FILTER)),
+     'WHERE operation_id BETWEEN {s} AND {e})'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
+
+
     (_insert_operation_category_operation,
      'DELETE FROM operation_category_operation '
-     'WHERE operation_id BETWEEN {s} AND {s}'.format(s=USER_FILTER)),
+     'WHERE operation_id BETWEEN {s} AND {e}'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
 
     (_insert_operation_platform, 'DELETE FROM operation_platform '
-                                 'WHERE operation_id BETWEEN {s} AND {s}'.format(s=USER_FILTER)),
+                                 'WHERE operation_id BETWEEN {s} AND {e}'.format(s=DATA_SOURCE, e=DATA_SOURCE)),
+
     (_insert_operation_form,
-     'DELETE FROM operation_form WHERE id BETWEEN {f} AND {f}'.format(f=FORM_ID)),
+     'DELETE FROM operation_form WHERE id BETWEEN {s} AND {e}'.format(
+         s=BASE_FORM_ID + 1, e=BASE_FORM_ID + 1 + len(ALL_OPS))),
 
     (_insert_operation_operation_form, 'DELETE FROM operation_operation_form '
-                                       'WHERE operation_id BETWEEN {s} AND {s}'.format(s=USER_FILTER)),
+                                       'WHERE operation_id BETWEEN {s} AND {e} '.format(
+                                           s=DATA_SOURCE, e=DATA_SOURCE)),
     (_insert_operation_form_translation,
-     'DELETE FROM operation_form_translation WHERE id BETWEEN {f} AND {f}'.format(f=FORM_ID)),
+     'DELETE FROM operation_form_translation WHERE id BETWEEN {s} AND {e}'.format(
+         s=BASE_FORM_ID + 1 , e=BASE_FORM_ID + 1 + len(ALL_OPS))),
 
-    (_insert_operation_form_field, """DELETE FROM operation_form_field
-         WHERE form_id BETWEEN {f} AND {f}""".format(f=FORM_ID)),
+     (_insert_operation_form_field, """DELETE FROM operation_form_field 
+          WHERE form_id BETWEEN {s} AND {e} """.format(
+              s=BASE_FORM_ID + 1, e=BASE_FORM_ID + 1 + len(ALL_OPS))),
+ 
+     (_insert_operation_form_field_translation,
+      'DELETE FROM operation_form_field_translation WHERE id IN (' +
+      'SELECT id FROM operation_form_field WHERE form_id BETWEEN {s} AND {e})'.format(
+          s=BASE_FORM_ID + 1, e=BASE_FORM_ID + 1 + len(ALL_OPS))),
 
-    (_insert_operation_form_field_translation,
-     'DELETE FROM operation_form_field_translation WHERE id IN (' +
-     'SELECT id FROM operation_form_field WHERE form_id BETWEEN {f} AND {f})'.format(f=FORM_ID)),
+
 ]
 
 
@@ -315,7 +349,6 @@ def upgrade():
     session = sessionmaker(bind=ctx.bind)()
     connection = session.connection()
     try:
-        op.add_column('workflow', sa.Column('publishing_status', sa.Enum('DISABLED', 'EDITING', 'PUBLISHED', name='PublishingStatusEnumType'), nullable=True))
         for cmd in all_commands:
             if isinstance(cmd[0], str):
                 connection.execute(cmd[0])
@@ -337,7 +370,6 @@ def downgrade():
 
     try:
         connection.execute('SET FOREIGN_KEY_CHECKS=0;')
-        op.drop_column('workflow', 'publishing_status')
         for cmd in reversed(all_commands):
             if isinstance(cmd[1], str):
                 connection.execute(cmd[1])
