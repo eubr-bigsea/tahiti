bc2e49e2e44c9a1109512d4c76ba0bfec66486dd
ericksunclair <ericksunclair@hotmail.com>
2023-07-05 11:06:27 -0300

    sourcecode detail api first version

--- tahiti/source_code_api.py
+++ tahiti/source_code_api.py
@@ -5,9 +5,9 @@ from flask import request, current_app, g
 from flask_babel import gettext
 from flask_restful import Resource
 from http import HTTPStatus
-
 from marshmallow.exceptions import ValidationError
-from tahiti.app_auth import requires_auth
+
+from tahiti.app_auth import requires_auth, requires_permission
 from tahiti.schema import *
 from tahiti.models import *
 from tahiti.util import translate_validation
@@ -54,6 +54,7 @@ class SourceCodeListApi(Resource):
         return result
 
     @requires_auth
+    @requires_permission('ADMINISTRATOR',)
     def post():
         result = {'status': 'ERROR',
                   'message': gettext("Missing json in the request body")}
@@ -94,3 +95,112 @@ class SourceCodeListApi(Resource):
 class SourceCodeDetailApi(Resource):
     """ REST API for a single instance of class SourceCode """
 
+    def __init__(self):
+            self.human_name = gettext('SourceCode')
+
+    @requires_auth
+    def get(self, sourcecode_id):
+
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug(gettext('Retrieving %s (id=%s)'), self.human_name,
+                      sourcecode_id)
+
+        sourcecode = SourceCode.query.get(sourcecode_id)
+        return_code = HTTPStatus.OK
+        if sourcecode is not None:
+            result = {
+                'status': 'OK',
+                'data': [SourceCodeItemResponseSchema().dump(
+                    sourcecode)]
+            }
+        else:
+            return_code = HTTPStatus.NOT_FOUND
+            result = {
+                'status': 'ERROR',
+                'message': gettext(
+                    '%(name)s not found (id=%(id)s)',
+                    name=self.human_name, id=sourcecode_id)
+            }
+
+        return result, return_code
+
+    @requires_auth
+    @requires_permission('ADMINISTRATOR',)
+    def delete(self, sourcecode_id):
+        return_code = HTTPStatus.NO_CONTENT
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug(gettext('Deleting %s (id=%s)'), self.human_name,
+                      sourcecode_id)
+        sourcecode = SourceCode.query.get(sourcecode_id)
+        if sourcecode is not None:
+            try:
+                db.session.delete(sourcecode)
+                db.session.commit()
+                result = {
+                    'status': 'OK',
+                    'message': gettext('%(name)s deleted with success!',
+                                       name=self.human_name)
+                }
+            except Exception as e:
+                result = {'status': 'ERROR',
+                          'message': gettext("Internal error")}
+                return_code = HTTPStatus.INTERNAL_SERVER_ERROR
+                if current_app.debug:
+                    result['debug_detail'] = str(e)
+                db.session.rollback()
+        else:
+            return_code = HTTPStatus.NOT_FOUND
+            result = {
+                'status': 'ERROR',
+                'message': gettext('%(name)s not found (id=%(id)s).',
+                                   name=self.human_name, id=sourcecode_id)
+            }
+        return result, return_code
+
+    @requires_auth
+    @requires_permission('ADMINISTRATOR',)
+    def patch(self, sourcecode_id):
+        result = {'status': 'ERROR', 'message': gettext('Insufficient data.')}
+        return_code = HTTPStatus.NOT_FOUND
+
+        if log.isEnabledFor(logging.DEBUG):
+            log.debug(gettext('Updating %s (id=%s)'), self.human_name,
+                      sourcecode_id)
+        if request.json:
+            request_schema = partial_schema_factory(
+                SourceCodeCreateRequestSchema)
+            # Ignore missing fields to allow partial updates
+            sourcecode = request_schema.load(request.json, partial=True)
+            response_schema = SourceCodeItemResponseSchema()
+            try:
+                sourcecode.id = sourcecode_id
+                sourcecode = db.session.merge(sourcecode)
+                db.session.commit()
+
+                if sourcecode is not None:
+                    return_code = HTTPStatus.OK
+                    result = {
+                        'status': 'OK',
+                        'message': gettext(
+                            '%(n)s (id=%(id)s) was updated with success!',
+                            n=self.human_name,
+                            id=sourcecode_id),
+                        'data': [response_schema.dump(
+                            sourcecode)]
+                    }
+            except ValidationError as e:
+                result= {
+                   'status': 'ERROR', 
+                   'message': gettext('Invalid data for %(name)s (id=%(id)s)',
+                                      name=self.human_name,
+                                      id=sourcecode_id),
+                   'errors': translate_validation(e.messages)
+                }
+            except Exception as e:
+                result = {'status': 'ERROR',
+                          'message': gettext("Internal error")}
+                return_code = 500
+                if current_app.debug:
+                    result['debug_detail'] = str(e)
+                db.session.rollback()
+        return result, return_code
