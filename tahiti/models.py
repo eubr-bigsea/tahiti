5ac72d24ea595aeefe040c6f254cdfe6a9b27eac
samuellipovetsky <samuellpvt@gmail.com>
2024-12-04 09:42:52 -0300

    new field

--- tahiti/models.py
+++ tahiti/models.py
@@ -656,7 +656,7 @@ class Pipeline(db.Model):
     periodicity = Column(String(50), nullable=False)
     periodicity_start = Column(Integer, nullable=False)
     periodicity_interval = Column(Integer)
-
+    run_creation_method = Column(String(50), default="scheduler", nullable=False)
     # Associations
     steps = relationship("PipelineStep",
                          cascade="all, delete-orphan")
