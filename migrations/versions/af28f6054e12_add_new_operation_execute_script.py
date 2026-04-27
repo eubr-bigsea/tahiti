f755c1ca28303934aa0df111d52912fa9178c357
Walter dos Santos Filho <waltersf@gmail.com>
2026-04-27 18:38:01 -0300

    Add execute script op

---
+++ migrations/versions/af28f6054e12_add_new_operation_execute_script.py
@@ -0,0 +1,176 @@
+"""Add new operation execute script
+
+Revision ID: af28f6054e12 
+Revises: f5c20165bdaf
+"""
+from alembic import context
+from alembic import op
+from sqlalchemy import Integer, String, Text, Boolean, UnicodeText
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import table, column
+from sqlalchemy.sql.sqltypes import UnicodeText
+
+# revision identifiers, used by Alembic.
+revision = 'af28f6054e12'
+down_revision = 'f5c20165bdaf'
+branch_labels = None
+depends_on = None
+
+# --------------------------------------------------------------
+# ATENTION: You must revise this auto-generated code.
+# Please, review offsets and commands before running a migration
+# and optionally, perform a database backup. If everything is 
+# OK, remove this comment.
+# --------------------------------------------------------------
+
+BASE_OP = 145
+
+def _insert_operation(conn):
+    tb = table('operation',
+                column('id', Integer), 
+                column('slug', String), 
+                column('enabled', Boolean), 
+                column('type', String), 
+                column('icon', String), 
+                column('css_class', String), 
+                column('doc_link', String))
+    columns = [c.name for c in tb.columns]
+    data = [
+      [BASE_OP + 1, 'execute-script', 1, 'TRANSFORMATION', '', '', '']
+    ]
+    rows = [dict(zip(columns, row)) for row in data]
+    op.bulk_insert(tb, rows)
+
+def _delete_operation(conn):
+    conn.execute(
+        'DELETE from operation WHERE id BETWEEN %s AND %s', 
+        BASE_OP + 1, BASE_OP + 1)
+
+def _insert_operation_translation(conn):
+    tb = table('operation_translation',
+                column('id', Integer), 
+                column('locale', String), 
+                column('name', String), 
+                column('description', String), 
+                column('label_format', String))
+    columns = [c.name for c in tb.columns]
+    data = [
+      [BASE_OP + 1, 'pt', 'Executar script externo', 'Permite executar script externo', ''],
+      [BASE_OP + 1, 'en', 'Execute external script', 'Allow execute an external script', ''],
+    ]
+    rows = [dict(zip(columns, row)) for row in data]
+    op.bulk_insert(tb, rows)
+
+def _delete_operation_translation(conn):
+    conn.execute(
+        'DELETE from operation_translation WHERE id BETWEEN %s AND %s', 
+        BASE_OP + 1, BASE_OP + 1)
+
+def _insert_operation_script(conn):
+    tb = table('operation_script',
+                column('id', Integer), 
+                column('type', String), 
+                column('enabled', Boolean), 
+                column('body', Text), 
+                column('operation_id', Integer))
+    columns = [c.name for c in tb.columns]
+    data = [
+      [None, 'JS_CLIENT', 1, '', BASE_OP + 1]
+    ]
+    rows = [dict(zip(columns, row)) for row in data]
+    op.bulk_insert(tb, rows)
+
+def _delete_operation_script(conn):
+    conn.execute(
+        'DELETE from operation_script WHERE operation_id BETWEEN %s AND %s', 
+        BASE_OP + 1, BASE_OP + 1)
+
+def _insert_operation_category_operation(conn):
+    tb = table('operation_category_operation',
+                column('operation_id', Integer), 
+                column('operation_category_id', Integer))
+    columns = [c.name for c in tb.columns]
+    data = [
+            [BASE_OP +1 , 1],
+            [BASE_OP +1 , 41],
+    ]
+    rows = [dict(list(zip(columns, row))) for row in data]
+    op.bulk_insert(tb, rows)
+
+def _delete_operation_category_operation(conn):
+    conn.execute('DELETE FROM operation_category_operation where operation_id BETWEEN %s AND %s',
+            BASE_OP + 1, BASE_OP + 1)
+
+def _insert_operation_platform(conn):
+    tb = table('operation_platform',
+                column('operation_id', Integer), 
+                column('platform_id', Integer))
+    columns = [c.name for c in tb.columns]
+    data = [
+            [BASE_OP + 1,  1000]
+    ]
+    rows = [dict(list(zip(columns, row))) for row in data]
+    op.bulk_insert(tb, rows)
+
+def _delete_operation_platform(conn):
+    conn.execute('DELETE FROM operation_platform where operation_id BETWEEN %s AND %s',
+            BASE_OP + 1, BASE_OP + 1)
+
+
+# -------------------------------------------------------
+
+def _execute(conn, cmd):
+    if isinstance(cmd, str):
+        conn.execute(cmd)
+    elif isinstance(cmd, list):
+        for row in cmd:
+            conn.execute(row)
+    else: # it's a method
+        cmd(conn)
+
+# -------------------------------------------------------
+
+def upgrade():
+    ctx = context.get_context()
+    session = sessionmaker(bind=ctx.bind)() 
+    conn = session.connection()
+    commands = [
+        _insert_operation,
+        _insert_operation_translation,
+        _insert_operation_script,
+        _insert_operation_category_operation,
+        _insert_operation_platform,
+    ]
+    try:
+        for cmd in commands:
+            _execute(conn, cmd)
+    except:
+        session.rollback()
+        raise
+    session.commit()
+
+
+def downgrade():
+    ctx = context.get_context()
+    session = sessionmaker(bind=ctx.bind)() 
+    conn = session.connection()
+
+    # Remove it if your DB doesn't support disabling FK checks
+    conn.execute('SET FOREIGN_KEY_CHECKS=0;')
+    commands = [
+        _delete_operation,
+        _delete_operation_translation,
+        _delete_operation_script,
+        _delete_operation_category_operation,
+        _delete_operation_platform,
+    ]
+
+    try:
+        for cmd in reversed(commands):
+            _execute(conn, cmd)
+    except:
+        session.rollback()
+        raise
+    # Remove it if your DB doesn't support disabling FK checks
+    conn.execute('SET FOREIGN_KEY_CHECKS=1;')
+    session.commit()
